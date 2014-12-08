a322acdff440e532d7d9597bacde24bf7f4c0df6
Mike <Airy>
2014-12-09 02:24:54 +0300

    Changes to comments association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :answer
-  belongs_to :question
+  belongs_to :commentable, polymorphic: true
 
   validates :body, presence: true, length: { maximum: 1000 }
+  validates :commentable_id, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
   it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:commentable_id) }
   it { should ensure_length_of(:body).is_at_most(1000) }
+
+  describe "question association" do
+    let(:question) { FactoryGirl.create(:question) }
+    before { @comment = question.comments.create(body:"Comment") }
+
+    it "should have right comment" do
+      expect(question.comments.first).to eq @comment
+    end
+
+    it "should have right question" do
+      expect(@comment.commentable).to eq question
+    end
+  end
+
+  describe "answer association" do
+    let(:answer) { FactoryGirl.create(:question) }
+    before { @comment = answer.comments.create(body:"Comment") }
+
+    it "should have right answer" do
+      expect(answer.comments.first).to eq @comment
+    end
+
+    it "should have right answer" do
+      expect(@comment.commentable).to eq answer
+    end
+  end
 end
