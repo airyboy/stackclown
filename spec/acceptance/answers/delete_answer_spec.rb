3bff31be416f5b81652ad49e07bfdcf124d7fcd9
Mike <Airy>
2014-12-24 21:36:47 +0300

    Finished answers deletion

--- spec/acceptance/answers/delete_answer_spec.rb
+++ spec/acceptance/answers/delete_answer_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'acceptance_helper'
+require_relative '../../acceptance/acceptance_helper'
 
 feature 'Deleting an answer', %q{
     In order to remove answer
@@ -6,16 +6,19 @@ feature 'Deleting an answer', %q{
     I want to be able to remove the answer
  } do
   given!(:user) { create(:foo_user) }
-  given!(:other_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given(:answer) { create(:answer, question: question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: user) }
+
+  given!(:other_user) { create(:user) }
+  given!(:other_question) { create(:question, user: other_user) }
+  given!(:other_answer) { create(:answer, question: other_question, user: other_user) }
 
   describe 'Registered user' do
     before do
       sign_in(user)
     end
 
-    scenario 'tries to remove an answer', js: true do
+    scenario 'tries to remove his answer', js: true do
       visit question_answers_path(question)
       within '.answers' do
         click_on 'x'
@@ -24,9 +27,17 @@ feature 'Deleting an answer', %q{
       end
     end
 
-    scenario "tries to remove other's answer" do
-      let!(:others_answer) { create(:answer, user: other_user, question: question)  }
+    scenario 'tries to remove his answer, but change his mind', js: true do
       visit question_answers_path(question)
+      within '.answers' do
+        click_on 'x'
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content answer.body
+      end
+    end
+
+    scenario "tries to remove other's answer" do
+      visit question_answers_path(other_question)
       within '.answers' do
         expect(page).not_to have_link 'x'
       end
@@ -35,7 +46,7 @@ feature 'Deleting an answer', %q{
 
 
   scenario 'Non-registered user tries to remove an answer' do
-    visit question_answers_path(question)
+    visit question_answers_path(other_question)
 
     expect(page).not_to have_link 'x'
   end
