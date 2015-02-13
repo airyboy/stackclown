80ae64eaa733d1177ee1251db7197aa2b332019d
Mike <Airy>
2015-02-13 16:28:37 +0800

    Fixed secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/home/deploy/stackclown'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_files, %w{config/database.yml config/private_pub.yml config/private_pub_thin.yml  .env}
+set :linked_files, %w{config/database.yml config/private_pub.yml config/private_pub_thin.yml .env}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -106,12 +106,12 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = "#{Rails.application.secrets.sorcery_twitter_key}"
   config.twitter.secret = "#{Rails.application.secrets.sorcery_twitter_secret}"
-  config.twitter.callback_url = 'http://lvh.me:3000/oauth/callback?provider=twitter'
+  config.twitter.callback_url = "#{Rails.application.secrets.sorcery_twitter_callback}"
   config.twitter.user_info_mapping = {:screen_name => 'screen_name'}
   #
   config.facebook.key = "#{Rails.application.secrets.sorcery_facebook_key}"
   config.facebook.secret = "#{Rails.application.secrets.sorcery_facebook_secret}"
-  config.facebook.callback_url = 'http://lvh.me:3000/oauth/callback?provider=facebook'
+  config.twitter.callback_url = "#{Rails.application.secrets.sorcery_facebook_callback}"
   config.facebook.user_info_mapping = {:email => 'email'}
   config.facebook.access_permissions = ['email']
   #
